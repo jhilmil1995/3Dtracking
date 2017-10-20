980ee22adce509d2e3ae0bdc7bb31222282ee4cf
Jake Ashmore <jacob.ashmore.stu@gtri.gatech.edu>
2017-10-20 17:01:45 -0400

    arduino analog read for each coil | python script to connect to arduino and read coil information

---
+++ arduino/coilReadSketch/coilReadSketch.ino
@@ -0,0 +1,45 @@
+int coil1 = 1;
+int coil2 = 2;
+int coil3 = 3;
+int coil4 = 4;
+
+double reading1;
+double reading2;
+double reading3;
+double reading4;
+
+static char outstr[32];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  char coil1_tag[] = "coil1";
+  char coil2_tag[] = "coil2";
+  char coil3_tag[] = "coil3";
+  char coil4_tag[] = "coil4";
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  reading1 = analogRead(coil1);
+  dtostrf(reading1, 10, 4, outstr);
+  strcat(outstr, reading1);
+  Serial.println(&outstr);
+
+  reading2 = analogRead(coil2);
+  dtostrf(reading2, 10, 4, outstr);
+  strcat(outstr, reading2);
+  Serial.println(&outstr);
+  
+  reading3 = analogRead(coil3);
+  dtostrf(reading3, 10, 4, outstr);
+  strcat(outstr, reading3);
+  Serial.println(&outstr);
+  
+  reading4 = analogRead(coil4);
+  dtostrf(reading4, 10, 4, outstr);
+  strcat(outstr, reading4);
+  Serial.println(&outstr);
+  
+  //delay(10);
+}
